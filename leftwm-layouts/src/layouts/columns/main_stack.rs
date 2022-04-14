f97a0240b24d9f7481f73ead07b37fce6c0985e7
Michael <3533648+hertg@users.noreply.github.com>
2022-04-14 23:46:49 +0200

    revert reserve-empty-space change

--- leftwm-layouts/src/layouts/columns/main_stack.rs
+++ leftwm-layouts/src/layouts/columns/main_stack.rs
@@ -1,9 +1,6 @@
 use std::cmp;
 
-use crate::{
-    geometry::{self, Rect, ReserveColumnSpace},
-    LayoutModifiers,
-};
+use crate::{geometry::{self, Rect}, LayoutModifiers};
 
 /// The `main_stack` column layout consists of a main column
 /// on the left, and a stack column on the right.
@@ -29,7 +26,7 @@ pub fn main_stack(window_count: usize, container: Rect, modifiers: &LayoutModifi
         container,
         modifiers.main_window_count,
         modifiers.main_size_percentage,
-        modifiers.reserve_column_space,
+        modifiers.reserve_empty_space,
     );
 
     if let Some(tile) = main_tile {
@@ -58,52 +55,31 @@ fn main_stack_columns(
     container: Rect,
     main_window_count: usize,
     main_size_percentage: f32,
-    reserve_column_space: ReserveColumnSpace,
+    reserve_empty_space: bool,
 ) -> (Option<Rect>, Option<Rect>) {
     let main_window_count = cmp::min(main_window_count, window_count);
     let stack_window_count = window_count.saturating_sub(main_window_count);
 
-    let main_exists = main_window_count > 0 || reserve_column_space.is_reserved();
-    let stack_exists = stack_window_count > 0 || reserve_column_space.is_reserved();
+    let main_exists = main_window_count > 0 || reserve_empty_space;
+    let stack_exists = stack_window_count > 0 || reserve_empty_space;
 
     let main_column = match (main_exists, stack_exists) {
         (false, _) => None,
-        (true, true) => {
-            let main_width = (container.w as f32 / 100.0 * main_size_percentage) as u32;
-            let offset_x = if stack_window_count == 0
-                && reserve_column_space.eq(&ReserveColumnSpace::ReserveAndCenter)
-            {
-                ((container.w - main_width) / 2) as i32
-            } else {
-                container.x
-            };
-            Some(Rect {
-                w: main_width,
-                x: offset_x,
-                ..container
-            })
-        }
+        (true, true) => Some(Rect {
+            w: (container.w as f32 / 100.0 * main_size_percentage) as u32,
+            ..container
+        }),
         (true, false) => Some(Rect { ..container }),
     };
 
     let stack_column = match (main_column, stack_exists) {
         (_, false) => None,
         (None, true) => Some(Rect { ..container }),
-        (Some(m), true) => {
-            let stack_width = container.w - m.w;
-            let offset_x = if main_window_count == 0
-                && reserve_column_space.eq(&ReserveColumnSpace::ReserveAndCenter)
-            {
-                ((container.w - stack_width) / 2) as i32
-            } else {
-                m.w as i32
-            };
-            Some(Rect {
-                x: offset_x,
-                w: stack_width,
-                ..container
-            })
-        }
+        (Some(m), true) => Some(Rect {
+            x: m.w as i32,
+            w: container.w - m.w,
+            ..container
+        }),
     };
 
     (main_column, stack_column)
@@ -122,13 +98,7 @@ mod tests {
 
     #[test]
     fn zero_windows() {
-        let rects = main_stack_columns(
-            0,
-            CONTAINER,
-            1,
-            60.0,
-            crate::geometry::ReserveColumnSpace::None,
-        );
+        let rects = main_stack_columns(0, CONTAINER, 1, 60.0, false);
         assert_eq!(rects.0, None);
         assert_eq!(rects.1, None);
     }
