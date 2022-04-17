17fea0cad4d09ba6069820d04cd02e570b121cd4
Michael <3533648+hertg@users.noreply.github.com>
2022-04-18 01:50:59 +0200

    current working state

--- leftwm-layouts/src/geometry/reserve_space.rs
+++ leftwm-layouts/src/geometry/reserve_space.rs
@@ -1,8 +1,12 @@
+use std::cmp;
+
+use super::{Rect, remainderless_division};
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum ReserveColumnSpace {
     None,
     Reserve,
-    ReserveAndCenter
+    ReserveAndCenter,
 }
 
 impl ReserveColumnSpace {
@@ -18,4 +22,32 @@ impl Default for ReserveColumnSpace {
     fn default() -> Self {
         ReserveColumnSpace::None
     }
+}
+
+fn shift_offsets(
+    full_width: u32,
+    main_column: Rect,
+    left_column: Option<Rect>,
+    right_column: Option<Rect>,
+    window_count: usize,
+    main_window_count: usize
+) {
+    let main_count = cmp::min(window_count, main_window_count);
+    let stack_count = window_count.saturating_sub(main_count);
+    let mut free_space = 0u32;
+    if main_count == 0 {
+        free_space += main_column.w;
+    }
+    if let Some(left) = left_column {
+        if stack_count == 0 {
+            free_space += left.w
+        }
+    }
+    if let Some(right) = right_column {
+        if stack_count == 1 {
+            free_space += right.w
+        }
+    }
+    let left_right_space = remainderless_division(free_space as usize, 2);
+    
 }
