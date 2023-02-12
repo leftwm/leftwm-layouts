72af591ef7ad73e7f89fe547e79538510b44791d
hertg <3533648+hertg@users.noreply.github.com>
2023-02-12 16:47:59 +0100

    update docs, update readme

--- leftwm-layouts/README.md
+++ leftwm-layouts/README.md
@@ -1,4 +1,281 @@
-# leftwm-layouts
-> Looking for the LeftWM repository? See [leftwm/leftwm](https://github.com/leftwm/leftwm).
+<div align="center">
+  <h1><strong>LeftWM Layouts</strong></h1>
+  <p>
+    <strong>Library providing fixed but parameterized window layout calculations</strong>
+  </p>
+  <p>
+    <a href="#">
+        <img alt="Libraries.io dependency status for GitHub repo" src="https://img.shields.io/librariesio/github/leftwm/leftwm-layouts" />
+    </a>
+    <a href="https://crates.io/crates/leftwm-layouts">
+        <img alt="Crates.io" src="https://img.shields.io/crates/v/leftwm-layouts">
+    </a>
+    <a href="https://crates.io/crates/leftwm-layouts">
+        <img alt="Crates.io" src="https://img.shields.io/crates/d/leftwm-layouts">
+    </a>
+  </p>
+</div>
 
-A library providing pre-defined but modifiable window layouts for window managers - specifically designed by and for LeftWM
+## Description
+
+This library encapsulates layout calculations from external dependencies and display servers,
+so that it can be used by window managers for X.Org, Wayland, or whatever else. It's all about
+splitting up rectangles, the library has no concept of "windows".
+
+Some default layouts are provided, but custom layouts can be defined. Custom layout
+definitions allow some flexibility but are still fairly limited by design, as the target
+audience of this library are **list-based** / **dynamic-tiling** window managers (the likes of `leftwm`, `dwm`, ...) as opposed to manual tilers like `i3`.
+
+## Features
+
+- Already provides widely known default layouts
+- Custom layouts can be defined
+- Supports multiple main windows
+- Ultrawide monitor friendly
+- Zero dependencies (*if you ignore serde :eyes:*)
+
+## Pre-defined Layouts
+
+The following layouts are already provided by the library.
+
+### Stack / Single-Column
+
+Those layouts have only a single stack and no main column.
+
+**Monocle**
+
+```txt
++-----------+
+|           |   only ever displays
+|           |   one window at
+|           |   maximum
++-----------+
+```
+
+**EvenHorizontal**
+
+```txt
++--+--+--+--+
+|  |  |  |  |
+|  |  |  |  |
+|  |  |  |  |
++--+--+--+--+
+```
+
+**EvenVertical**
+
+```txt
++-----------+
+|-----------|
+|-----------|
+|-----------|
++-----------+
+```
+
+**Grid**
+
+```txt
++-----+-----+   +---+---+---+   +---+---+---+   +---+---+---+
+|     |     |   |   |   |   |   |   |   |   |   |   |   |   |
+|     |     |   |   |   |   |   |   |   |   |   |   |   +---+
++-----+-----+   |   +---+---+   +---+---+---+   +---+---|   |
+|     |     |   |   |   |   |   |   |   |   |   |   |   +---+
+|     |     |   |   |   |   |   |   |   |   |   |   |   |   |
++-----+-----+   +---+---+---+   +---+---+---+   +---+---+---+
+  4 windows       5 windows       6 windows       7 windows
+```
+
+### Main and Stack / Two-Column
+
+Those layouts have a main and one stack column
+
+**MainAndDeck**
+
+```txt
++-------+-----+
+|       |     |   only ever displays
+|       |     |   two windows at
+|       |     |   maximum
++-------+-----+
+  main   stack
+```
+
+**MainAndVertStack**
+
+```txt
++-------+-----+
+|       |     |
+|       +-----+
+|       |     |
++-------+-----+
+  main   stack
+```
+
+**MainAndHorizontalStack**
+
+```txt
++-------+--+--+
+|       |  |  |
+|       |  |  |
+|       |  |  |
++-------+--+--+
+  main   stack
+```
+
+**RightMainAndVertStack**
+
+```txt
++-----+-------+
+|     |       |
++-----+       |
+|     |       |
++-----+-------+
+ stack   main
+```
+
+**Fibonacci**
+
+```txt
++-------+-----+
+|       |     |
+|       +--+--+
+|       |--|  |
++-------+--+--+
+  main   stack
+```
+
+**Dwindle**
+
+```txt
++-------+-----+
+|       |     |
+|       +--+--+
+|       |  |--|
++-------+--+--+
+  main   stack
+```
+
+### Center Main / Three-Column
+
+Those layouts have the main column in the center
+and two stack columns surrounding it.
+
+**CenterMain**
+
+```text
++-----+-----------+-----+
+|     |           |     |
+|     |           +-----+
+|     |           |     |
+|     |           +-----+
+|     |           |     |
++-----+-----------+-----+
+  1st      main     2nd
+ stack             stack
+
++-----------+-----------+
+|           |           |
+|           |           |  unoccupied
+|           |           |  space is
+|           |           |  taken over
+|           |           |
++-----------+-----------+
+  1st stack      main
+
++-----------------------+
+|                       |
+|                       |  unoccupied
+|                       |  space is
+|                       |  taken over
+|                       |
++-----------------------+
+           main
+```
+
+**CenterMainBalanced**
+
+```text
++-----+-----------+-----+
+|     |           |     |
+|     |           |     |
++-----+           +--+--+
+|  |__|           |  |__|
+|  |  |           |  |  |
++-----+-----------+--+--+
+  1st      main     2nd
+ stack             stack
+
++-----------+-----------+
+|           |           |
+|           |           |  unoccupied
+|           |           |  space is
+|           |           |  taken over
+|           |           |
++-----------+-----------+
+  1st stack      main
+
++-----------------------+
+|                       |
+|                       |  unoccupied
+|                       |  space is
+|                       |  taken over
+|                       |
++-----------------------+
+           main
+```
+
+**CenterMainFluid**
+
+```text
+ 1st               2nd
+ stack     main    stack
++-----+-----------+-----+
+|     |           |     |
+|     |           +-----+
+|     |           |     |
+|     |           +-----+
+|     |           |     |
++-----+-----------+-----+
+  1st      main     2nd
+ stack             stack
+
++-----+-----------+-----+
+|     |           |.....|
+|     |           |.....|  unoccupied
+|     |           |.....|  space is
+|     |           |.....|  reserved
+|     |           |.....|
++-----+-----------+-----+
+  1st      main
+ stack
+
++-----+-----------+-----+
+|.....|           |.....|
+|.....|           |.....|  unoccupied
+|.....|           |.....|  space is
+|.....|           |.....|  reserved
+|.....|           |.....|
++-----+-----------+-----+
+           main
+```
+
+---
+
+## Build
+
+### Build and test the project
+
+> **Info** Requires Rust >= 1.56.0
+
+```shell
+make
+```
+
+### Run the demo application
+
+> **Info** Running the demo requires you to have GTK (`gtk3`) and pango (`?`) installed
+
+```shell
+make dev
+```
+
--- leftwm-layouts/src/layouts/defaults.rs
+++ leftwm-layouts/src/layouts/defaults.rs
@@ -21,6 +21,17 @@ const CENTER_MAIN: &str = "CenterMain";
 const CENTER_MAIN_BALANCED: &str = "CenterMainBalanced";
 const CENTER_MAIN_FLUID: &str = "CenterMainFluid";
 
+/// Layout which gives each window full height, but splits the workspace width among them all.
+/// This layout has only one stack and no main column.
+/// The stack is split in a [`Split::Vertical`] pattern (resulting in a horizontal stack).
+///
+/// ```txt
+/// +--+--+--+--+
+/// |  |  |  |  |
+/// |  |  |  |  |
+/// |  |  |  |  |
+/// +--+--+--+--+
+/// ```
 pub fn even_horizontal() -> Layout {
     Layout {
         name: EVEN_HORIZONTAL.to_string(),
@@ -36,6 +47,17 @@ pub fn even_horizontal() -> Layout {
     }
 }
 
+/// Layout which gives each window full width, but splits the workspace height among them all.
+/// This layout has only one stack and no main column.
+/// The stack is split in a [`Split::Horizontal`] pattern (resulting in a vertical stack).
+///
+/// ```txt
+/// +-----------+
+/// |-----------|
+/// |-----------|
+/// |-----------|
+/// +-----------+
+/// ```
 pub fn even_vertical() -> Layout {
     Layout {
         name: EVEN_VERTICAL.to_string(),
@@ -48,6 +70,16 @@ pub fn even_vertical() -> Layout {
     }
 }
 
+/// Layout which gives only one window with the full real estate. A monocle mode.
+/// This layout has only one stack and no main column, with the stack not splitting at all.
+///
+/// ```txt
+/// +-----------+
+/// |           |
+/// |           |
+/// |           |
+/// +-----------+
+/// ```
 pub fn monocle() -> Layout {
     Layout {
         name: MONOCLE.to_string(),
@@ -63,6 +95,29 @@ pub fn monocle() -> Layout {
     }
 }
 
+/// Layout which splits the workspace in a [`Split::Grid`] pattern.
+/// It will prioritize creating a new column instead of a row.
+/// This layout has only one stack and no main column.
+///
+/// ```txt
+/// +-----+-----+   +---+---+---+
+/// |     |     |   |   |   |   |
+/// |     |     |   |   |   |   |
+/// +-----+-----+   |   +---+---+
+/// |     |     |   |   |   |   |
+/// |     |     |   |   |   |   |
+/// +-----+-----+   +---+---+---+
+///   4 windows       5 windows
+///
+/// +---+---+---+   +---+---+---+
+/// |   |   |   |   |   |   |   |
+/// |   |   |   |   |   |   +---+
+/// +---+---+---+   +---+---|   |
+/// |   |   |   |   |   |   +---+
+/// |   |   |   |   |   |   |   |
+/// +---+---+---+   +---+---+---+
+///   6 windows       7 windows
+/// ```
 pub fn grid() -> Layout {
     Layout {
         name: GRID.to_string(),
@@ -78,6 +133,17 @@ pub fn grid() -> Layout {
     }
 }
 
+/// Layout which splits the workspace into two columns (main and stack).
+/// The stack is split in a [`Split::Horizontal`] pattern (resulting in a vertical stack).
+///
+/// ```txt
+/// +-------+-----+
+/// |       |     |
+/// |       +-----+
+/// |       |     |
+/// +-------+-----+
+///   main   stack
+/// ```
 pub fn main_and_vert_stack() -> Layout {
     Layout {
         name: MAIN_AND_VERT_STACK.to_string(),
@@ -90,6 +156,17 @@ pub fn main_and_vert_stack() -> Layout {
     }
 }
 
+/// Layout which splits the workspace into two columns (main and stack).
+/// The stack is split in a [`Split::Vertical`] pattern (resulting in a horizontal stack).
+///
+/// ```txt
+/// +-------+--+--+
+/// |       |  |  |
+/// |       |  |  |
+/// |       |  |  |
+/// +-------+--+--+
+///   main   stack
+/// ```
 pub fn main_and_horizontal_stack() -> Layout {
     Layout {
         name: MAIN_AND_HORIZONTAL_STACK.to_string(),
@@ -105,6 +182,18 @@ pub fn main_and_horizontal_stack() -> Layout {
     }
 }
 
+/// Layout which splits the workspace into two columns (main and stack),
+/// with the main column being on the right side.
+/// The stack is split in a [`Split::Horizontal`] pattern (resulting in a vertical stack).
+///
+/// ```txt
+/// +-----+-------+
+/// |     |       |
+/// +-----+       |
+/// |     |       |
+/// +-----+-------+
+///  stack   main
+/// ```
 pub fn right_main_and_vert_stack() -> Layout {
     Layout {
         name: RIGHT_MAIN_AND_VERT_STACK.to_string(),
@@ -118,6 +207,17 @@ pub fn right_main_and_vert_stack() -> Layout {
     }
 }
 
+/// Layout which splits the workspace into two columns (main and stack).
+/// The stack is split in a [`Split::Fibonacci`] pattern.
+///
+/// ```txt
+/// +-------+-----+
+/// |       |     |
+/// |       +--+--+
+/// |       |--|  |
+/// +-------+--+--+
+///   main   stack
+/// ```
 pub fn fibonacci() -> Layout {
     Layout {
         name: FIBONACCI.to_string(),
@@ -133,6 +233,17 @@ pub fn fibonacci() -> Layout {
     }
 }
 
+/// Layout which splits the workspace into two columns (main and stack).
+/// The stack is split in a [`Split::Dwindle`] pattern.
+///
+/// ```txt
+/// +-------+-----+
+/// |       |     |
+/// |       +--+--+
+/// |       |  |--|
+/// +-------+--+--+
+///   main   stack
+/// ```
 pub fn dwindle() -> Layout {
     Layout {
         name: DWINDLE.to_string(),
@@ -148,6 +259,17 @@ pub fn dwindle() -> Layout {
     }
 }
 
+/// Layout similar to monocle, but with a non-splitting main column.
+/// Never displays more than two windows at once.
+///
+/// ```txt
+/// +-------+-----+
+/// |       |     |
+/// |       |     |
+/// |       |     |
+/// +-------+-----+
+///   main   stack
+/// ```
 pub fn main_and_deck() -> Layout {
     Layout {
         name: MAIN_AND_DECK.to_string(),
@@ -166,26 +288,48 @@ pub fn main_and_deck() -> Layout {
     }
 }
 
+/// Layout which splits the workspace into three columns (stack | main | second stack).
+///
+/// * Puts first N (`main_window_count`) windows into middle (main) column
+/// * Puts second window into left (stack) column
+/// * Puts rest of windows into right (second stack) column
+///
+/// *Note: The space of unoccupied columns will be taken over, see [`Reserve::None`] for details.*
+///
+/// ```text
+///  1st               2nd
+///  stack     main    stack
+/// +-----+-----------+-----+
+/// |     |           |  3  |
+/// |     |           +-----+
+/// |  2  |     1     |  4  |
+/// |     |           +-----+
+/// |     |           |  5  |
+/// +-----+-----------+-----+
+///
+/// +-----------+-----------+
+/// |           |           |
+/// |           |           |  unoccupied
+/// |     2     |     1     |  space is
+/// |           |           |  taken over
+/// |           |           |
+/// +-----------+-----------+
+///
+/// +-----------------------+
+/// |                       |
+/// |                       |  unoccupied
+/// |           1           |  space is
+/// |                       |  taken over
+/// |                       |
+/// +-----------------------+
+/// ```
 pub fn center_main() -> Layout {
     Layout {
         name: CENTER_MAIN.to_string(),
         columns: Columns {
             main: Some(Main::default()),
-            stack: Stack::default(),
-            second_stack: Some(SecondStack::default()),
-            ..Default::default()
-        },
-        ..Default::default()
-    }
-}
-
-pub fn center_main_balanced() -> Layout {
-    Layout {
-        name: CENTER_MAIN_BALANCED.to_string(),
-        columns: Columns {
-            main: Some(Main::default()),
             stack: Stack {
-                split: Some(Split::Dwindle),
+                split: None,
                 ..Default::default()
             },
             second_stack: Some(SecondStack::default()),
@@ -195,6 +339,80 @@ pub fn center_main_balanced() -> Layout {
     }
 }
 
+/// Layout which splits the workspace into three columns (stack | main | second stack).
+///
+/// * Puts first N (`main_window_count`) windows into middle (main) column
+/// * Distributes rest of windows between left and right column and splits the stacks in a [`Split::Dwindle`] pattern
+///
+/// *Hint: When dynamically adding windows, the windows already present may change the stack they are on.
+/// That is because leftwm-layouts will keep them in order of `main` -> `1st stack` -> `2nd stack` instead
+/// of going back and forth between 1st and second stack when going down the list of windows*
+///
+/// ```text
+///  1st               2nd
+///  stack     main    stack
+/// +-----+-----------+-----+
+/// |     |           |     |
+/// |     |           |     |
+/// +-----+           +--+--+
+/// |  |__|           |  |__|
+/// |  |  |           |  |  |
+/// +-----+-----------+--+--+
+/// ```
+pub fn center_main_balanced() -> Layout {
+    Layout {
+        name: CENTER_MAIN_BALANCED.to_string(),
+        columns: Columns {
+            main: Some(Main::default()),
+            stack: Stack {
+                split: Some(Split::Dwindle),
+                ..Default::default()
+            },
+            second_stack: Some(SecondStack {
+                split: Split::Dwindle,
+                ..Default::default()
+            }),
+            ..Default::default()
+        },
+        ..Default::default()
+    }
+}
+
+/// Layout which splits the workspace into three columns (stack | main | second stack).
+/// The layout is similar to `CenterMain`, but the space of unoccupied columns will be reserved,
+/// see [`Reserve::Reserve`] for details.
+///
+/// * Puts first N (`main_window_count`) windows into middle (main) column
+/// * Puts second window into left (stack) column
+/// * Puts rest of windows into right (second stack) column
+///
+/// ```text
+///  1st               2nd
+///  stack     main    stack
+/// +-----+-----------+-----+
+/// |     |           |  3  |
+/// |     |           +-----+
+/// |  2  |     1     |  4  |
+/// |     |           +-----+
+/// |     |           |  5  |
+/// +-----+-----------+-----+
+///
+/// +-----+-----------+-----+
+/// |.....|           |.....|
+/// |.....|           |.....|  unoccupied
+/// |.....|     1     |.....|  space is
+/// |.....|           |.....|  reserved
+/// |.....|           |.....|
+/// +-----+-----------+-----+
+///
+/// +-----+-----------+-----+
+/// |     |           |.....|
+/// |     |           |.....|  unoccupied
+/// |  2  |     1     |.....|  space is
+/// |     |           |.....|  reserved
+/// |     |           |.....|
+/// +-----+-----------+-----+
+/// ```
 pub fn center_main_fluid() -> Layout {
     Layout {
         name: CENTER_MAIN_FLUID.to_string(),
